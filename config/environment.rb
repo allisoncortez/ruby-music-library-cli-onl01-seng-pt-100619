d05b2579c112d969b874a197200d47b4514581c0
allison cortez <allizetorc@gmail.com>
2019-11-19 20:39:28 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,9 @@ module Concerns
       self.all.find {|song| song.name == name}
     end
     
+    def find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
     
 end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -50,4 +50,17 @@ class Song
     find_by_name(name) || create(name)
   end
   
+  def self.new_from_filename(filename)
+    split_file = filename.split(" - ")
+    artist_name, song_name, genre_name = split_file[0], split_file[1], split_file[2].gsub(".mp3", "")
+    
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    Song.new(song_name, artist, genre)
+  end
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+  
 end
